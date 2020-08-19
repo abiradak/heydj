fff1c78763e4c0f8281ae9de8009bd3ffef85b25
Abir Adak <abir10.wis@gmail.com>
2020-08-19 12:40:00 +0530

    hello

--- src/app/pages/nowplay/nowplay.page.ts
+++ src/app/pages/nowplay/nowplay.page.ts
@@ -9,12 +9,27 @@ import { ApiGenerateService } from '../../api-generate.service';
   styleUrls: ['./nowplay.page.scss'],
 })
 
+
 export class NowplayPage {
+  @ViewChild('player', { static: false }) public playerElementRef:  ElementRef;
+
+  isPlaying = false;
+  isLoading = false;
+  currentTime = 0;
+  duration = 0;
+  private _player: HTMLAudioElement;
+
+
+
+
   response = {};
   start: any;
   startTime: any;
   endtime: any;
   audio: HTMLAudioElement;
+  contentUrl = [];
+  isPlaying = false;
+  showPlay = false;
 
   constructor(
     private router : Router,
@@ -29,6 +44,11 @@ export class NowplayPage {
     this.subdetails();
   }
 
+  ngAfterViewInit(): void {
+    this._player = this.playerElementRef.nativeElement;
+    this._bindPlayerEvents();
+  }
+
   back() {
     this.router.navigate(['subscriptions']);
   }
@@ -36,7 +56,7 @@ export class NowplayPage {
     const id = this.route.snapshot.paramMap.get('id');
     this.apiGenerate.sendHttpCallWithToken('' , `/api/subscription/${id}` ,'get').subscribe((success) => {
       console.log('response coming >>>>>>>' , success);
-      
+      this.contentUrl = success.contents
       const starting = success.startFrom.split("T");
       this.start = starting[0];
       const startingTime = starting[1].split("+");
@@ -59,18 +79,36 @@ export class NowplayPage {
   }
 
   async playContent(content) {
-    console.log('content >>>>>>', content);
+    this.isPlaying = true;
+    this.showPlay = true;
     this.audio = new Audio();
     this.audio.src = content;
     this.audio.load();
+    this.audio.currentTime = 0;
     this.audio.play();
   }
   
   pause() {
+    this.showPlay = false;
     this.audio.pause();
   }
 
+  resume() {
+    this.showPlay = true;
+    this.audio.play();
+  }
+
+  // replay() {
+  //   this.isReplay = 
+  //   this.audio.currentTime = 0;
+  //   this.audio.play();
+  // }
+
+  endAudio() {
+    this.audio.e
+  }
+
   seek(){
-    this.audio.fastSeek(3);
+    this.audio.fastSeek(10);
   }
 }
